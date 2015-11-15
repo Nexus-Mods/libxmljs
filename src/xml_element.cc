700040c111d1a8ff1373440d4e79b19498851abf
Riley Martinez-Lynch <oss+libxmljs@teleological.net>
2015-11-15 13:45:13 -0500

    Merge `add_cdata` into `add_child`

--- src/xml_element.cc
+++ src/xml_element.cc
@@ -146,7 +146,7 @@ NAN_METHOD(XmlElement::AddCData) {
       xmlNewCDataBlock(element->xml_obj->doc, (const xmlChar *)content,
                        xmlStrlen((const xmlChar *)content));
 
-  element->add_cdata(elem);
+  element->add_child(elem);
   return info.GetReturnValue().Set(info.Holder());
 }
 
